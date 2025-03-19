4a88a4d7198bf7a1f2e241ad60730e1ad4d28f15
Mpho Matli <mphojames9@gmail.com>
2025-03-19 12:20:15 +0200

    update About us sec and marging css home page files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 // src/App.jsx
-import Header from './components/Home/Header';
-import Hero from './components/Home/Hero';
-import BookingForm from './components/Home/BookingForm';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './components/home/HomePage';
+import HomePage from './components/home/HomePage';
+import AboutPage from './components/about/About';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Hero />
-      <BookingForm />
+      <Routes>
+        <Route path="/hotel-bookings" element={<HomePage />} />
+        <Route path="/hotel-bookings/about" element={<AboutPage />} />
+      </Routes>
     </div>
   );
 }
